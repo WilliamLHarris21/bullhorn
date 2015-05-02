8c92aaffaf77ab0baac2ff4fc7618b709fc1b259
Williamlharris21 <williamlharris21@gmail.com>
2015-05-02 01:38:33 +0000

    added final touches

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150501032641) do
+ActiveRecord::Schema.define(version: 20150502002145) do
+
+  create_table "moos", force: true do |t|
+    t.text     "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "user_id"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
